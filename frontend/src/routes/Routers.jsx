38f8aaea08d9894e328e710835c9c64783d6c0b0
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 00:17:36 +0000

    short

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+// all pages
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Login from '../pages/Login'
+import Services from '../pages/Services'
+import Signup from '../pages/Signup'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
+
+
 const Routers = () => {
   return (
     <div>Routers</div>
