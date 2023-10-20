2806456c9061b5bffec17792ba179dc7460642c7
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 01:18:14 +0000

    short

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef} from 'react'
 import logo from "../../assets/images/logo.png"
-import {navLinks, Link} from 'react-router-dom'
+import {navLink, Link, NavLink} from 'react-router-dom'
 
 const navLinks = [
   {
@@ -34,7 +34,20 @@ const Header = () => {
           {/* =================== menu ==================== */}
           <div className="navigation">
             <ul className="menu flex items-center gap-[2.7rem]">
-
+            {navLink.map((link,index) => ( 
+              <li key={index}>
+                  <NavLink
+                   to={link.path} 
+                   className={navClass => 
+                      navClass.isActive 
+                        ? "text-primaryColor text-[16px] leading-7 font-[600]" 
+                        : "text-textColor text-[16px] leading-7 font-[500]"
+                      } 
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+            ))}
             </ul>
           </div>
         </div>
