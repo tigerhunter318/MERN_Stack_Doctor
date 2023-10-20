c5e83e484f7ec47caa345ab91e423377ff0a498d
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-20 14:01:01 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      {/* =================== hero section ==================== */}
+      <>
+        <section className="hero__section pt-[60px] 2xl:h-[800px]">
+          <div className="container">
+            <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+
+              {/* =================== hero section ==================== */}
+    
+
+            </div>
+          </div>
+        </section>
+      </>
+    </>
   )
 }
 
