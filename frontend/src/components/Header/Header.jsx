42871a3205e8239e2b9143c8fd64e86170386f16
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 12:05:36 +0000

    short

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Header
---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,26 @@
+// all pages
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Login from '../pages/Login'
+import Services from '../pages/Services'
+import Signup from '../pages/Signup'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
+
+
+import { Routes, Route } from 'react-router-dom'
+
+const Routers = () => {
+  return <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/home" element={<Home/>} />
+    <Route path="/doctors" element={<Doctors/>} />
+    <Route path="/doctors/:id" element={<DoctorsDetails/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/register" element={<Signup/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/services" element={<Services/>} />
+  </Routes>
+}
+
+export default Routers
