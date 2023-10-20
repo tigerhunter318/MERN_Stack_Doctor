98dc2e7906394c1b8871f230947b37e5f3aa85a7
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-20 00:06:26 +0000

    short

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+        <Header />
+        <main>
+            <Routers />
+        </main>
+        <Footer />
+    </>
   )
 }
 
