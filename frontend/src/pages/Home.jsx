c6d724c23fb64d93f96fbc72e59d396077caeef8
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 20:23:25 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,12 @@ const Home = () => {
               <div>
                 <div className="lg:w-[570px]">
                   <h1 className="text-[36px] leading-[46px] text-headingColor
-                   font-[880] md:text-[60px] md:leading-[70px]">We help patients live a healthy, longer life.</h1>
+                   font-[880] md:text-[60px] md:leading-[70px]">We help patients live a healthy, longer life.
+                  </h1>
+                  <p className="text__para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique iusto praesentium 
+                    quaerat magni dolorum quia illum pariatur facere, cumque, facilis quibusdam ducimus quod 
+                    ipsam culpa, cum earum ex exercitationem nesciunt.
+                  </p>
                 </div>
               </div>
 
