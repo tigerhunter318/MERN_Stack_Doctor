4ca190b4142c3d1da8c7db861ad73d18ae3302bb
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 00:57:47 +0000

    short

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import logo from "../../assets/images/logo.png"
 
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+]
+
 const Header = () => {
   return (
     <header className="header flex items-center">
       <div className="container">
         <div className="flex items-center justify-between">
-          {/* ======== logo ======= */}
+          {/* =================== logo ==================== */}
           <div>
             <img src={logo} alt="" />
+          </div>
+
+          {/* =================== menu ==================== */}
+          <div className="navigation">
+            <ul className="menu flex items-center gap-[2.7rem]">
+
+            </ul>
           </div>
         </div>
       </div>
