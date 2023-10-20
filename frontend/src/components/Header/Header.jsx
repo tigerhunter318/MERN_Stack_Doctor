bd35ffda5fed7efd16c0cfa4a08a1beed90aa7d4
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 00:53:01 +0000

    short

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import logo from "../../assets/images/logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* ======== logo ======= */}
+          <div>
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
