d0e833a9ffb4764bf4e1feb24fb82a8d42f285af
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 14:07:01 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,12 @@ const Home = () => {
             <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
 
               {/* =================== hero section ==================== */}
-    
+              <div>
+                <div className="lg:w-[570px]">
+                  <h1 className="text-[36px] leading-[46px] text-headingColor
+                   font-[880] md:text-[60px] md:leading-[70px]">We help patients live a healthy, longer life.</h1>
+                </div>
+              </div>
 
             </div>
           </div>
