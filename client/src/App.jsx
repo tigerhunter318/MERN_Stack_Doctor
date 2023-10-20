c9f0634a273e82899041652acff8752d2ba983f9
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-20 12:25:23 +0000

    remove

--- client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import Layout from "./layout/Layout";
-
-function App() {
-  return <Layout />
-}
-
-export default App
--- client/src/pages/Doctors/Doctors.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Doctors = () => {
-  return (
-    <div>Doctors</div>
-  )
-}
-
-export default Doctors
