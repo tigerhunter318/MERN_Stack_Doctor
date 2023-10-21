1cc05f37199562be83e2f3b34b2d7a41e6e44027
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-21 00:56:49 +0000

    shortt

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,21 @@ const Home = () => {
 
                   <button className="btn">Request an Appointment</button>
                 </div>
+
+                 {/* =================== hero section ==================== */}
+                 <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center 
+                 gap-5 lg:gap-[30px]">
+
+                    <div>
+                      <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] 
+                        font-[700] text-headingColor ">
+                        30+
+                      </h2>
+                    </div>
+                 </div>
               </div>
+
+
 
             </div>
           </div>
