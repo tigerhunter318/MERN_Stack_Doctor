dc42e8826257ab6c337e5955dfb981e5508866d8
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-24 23:07:28 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import heroImg01 from "../assets/images/hero-img01.png"
+import heroImg02 from "../assets/images/hero-img02.png"
+import heroImg03 from "../assets/images/hero-img03.png"
+
+
 const Home = () => {
   return (
     <>
       {/* =================== hero section ==================== */}
-      <>
+      
         <section className="hero__section pt-[60px] 2xl:h-[800px]">
           <div className="container">
             <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
 
-              {/* =================== hero section ==================== */}
+              {/* =================== hero content ==================== */}
               <div>
                 <div className="lg:w-[570px]">
                   <h1 className="text-[36px] leading-[46px] text-headingColor
@@ -23,7 +28,7 @@ const Home = () => {
                   <button className="btn">Request an Appointment</button>
                 </div>
 
-                 {/* =================== hero section ==================== */}
+                 {/* =================== hero counter ==================== */}
                  <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center 
                  gap-5 lg:gap-[30px]">
 
@@ -53,18 +58,26 @@ const Home = () => {
                       <span className="w-[100px] h-2 bg-blue-500 rounded-full block mt-[-14px]"></span>
                       <p className='text__para'>Patient Satisfaction</p>
                     </div>
-
-                    
-
-
                  </div>
               </div>
- 
+
+
+
+              {/* =================== hero content ==================== */}
+              <div className="flex gap-[30px] justify-end">
+                <div>
+                  <img className="w-full" src={heroImg01} alt="" />
+                </div>
+                <div className="mt-[30px]">
+                  <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                  <img src={heroImg03} alt="" className="w-full" />
+                </div>
+              </div>
 
             </div>
           </div>
         </section>
-      </>
+      
     </>
   )
 }
