bacfbcd2f5ebd0a22acd43924f5b821929dfa329
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-24 23:40:00 +0000

    short

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 import heroImg01 from "../assets/images/hero-img01.png"
 import heroImg02 from "../assets/images/hero-img02.png"
@@ -91,11 +92,25 @@ const Home = () => {
 
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+
             <div className="py-[30px] px-5">
               <div className="flex items-center justify-center">
                 <img src={icon01} alt="" />
               </div>
+
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Doctor</h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care. 
+                  From the lab to the clinic
+                </p>
+
+                <Link to='/doctors' className="w-[">
+
+                </Link>
+              </div>
             </div>
+
           </div>
 
         </div>
