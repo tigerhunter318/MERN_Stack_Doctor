6385f5074840130fdc500950fcf85c0daf910196
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-25 00:26:55 +0000

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -115,6 +115,48 @@ const Home = () => {
               </div>
             </div>
 
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon01} alt="" />
+              </div>
+
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Doctor</h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care. 
+                  From the lab to the clinic
+                </p>
+
+                <Link 
+                  to='/doctors' 
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] 
+                  mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                  <BsArrowRight className=" group-hover: text-white w-12 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon01} alt="" />
+              </div>
+
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">Find a Doctor</h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  World-class care for everyone. Our health System offers unmatched, expert health care. 
+                  From the lab to the clinic
+                </p>
+
+                <Link 
+                  to='/doctors' 
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] 
+                  mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+                  <BsArrowRight className=" group-hover: text-white w-12 h-5" />
+                </Link>
+              </div>
+            </div>
+
           </div>
 
         </div>
