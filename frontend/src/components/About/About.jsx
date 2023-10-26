cf4a8b4aa166169318d89ece9e0a65f1353a05e0
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-26 16:33:58 +0000

    short

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import aboutImg from '../../assets/images/about.png'
+
 const About = () => {
   return (
     <section>
@@ -7,10 +9,13 @@ const About = () => {
         <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
 
             {/*================= about img =============*/}
-            <div className="relative"></div>
+            <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1 ">
+                <img src={aboutImg} alt="" />
+                <div className="absolute"></div>
+            </div>
 
 
-            
+
         </div>
       </div>  
     </section>
