5e73ef0c1d3eecbd96ac31f649b8f30e9d27c051
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-26 21:52:17 +0000

    short

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -6,8 +6,16 @@ const ServiceCard = ({item, index}) => {
 
   return (
     <div className="py-[30px] px-3 lg:px-5">
-      <h2 className="text-[26px] leading-9 text-headingColor font-[700]">{name}</h2>
-      <p className="text-[16px] leading-7 font-[400] text-textColor mt-4 ">{desc}</p>
+      <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
+        {name}
+      </h2>
+      <p className="text-[16px] leading-7 font-[400] text-textColor mt-4 ">
+        {desc}
+      </p>
+
+      <div className="flex items-center justify-between mt-[30px] ">
+        
+      </div>
     </div>
   )
 }
