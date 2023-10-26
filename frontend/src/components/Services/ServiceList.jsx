3a36314b71eee5f94e756eaaab2d5feb269954ff
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-26 21:29:54 +0000

    short

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -6,7 +6,9 @@ import ServiceCard from './ServiceCard'
 const ServiceList = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-       {services.map((item, index ) => <ServiceCard /> )}
+       {services.map((item, index ) => (
+          <ServiceCard item={item} index={index} key={index} />
+        ))}
     </div>
      
   )
