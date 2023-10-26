f513d30b1c5e8101fc89ae4a55a70dd6aacc461a
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-26 21:42:39 +0000

    short

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const ServiceCard = () => {
+const ServiceCard = ({item, index}) => {
+
+  const {name, desc, bgColor, textColor} = item
+
   return (
-    <div>ServiceCard</div>
+    <div className="py-[30px] py-3 lg:px-5">
+      <h2 className="text-[26px] leading-9 text-headingColor font-[700]">{name}</h2>
+      <p className="text-[16px] leading-7 font-[400] text-textColor mt-4 ">{desc}</p>
+    </div>
   )
 }
 
