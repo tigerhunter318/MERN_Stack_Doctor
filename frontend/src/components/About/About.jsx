df1d9c07dd5b4a67b4a4f2f1428c5404af64aef6
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-26 15:54:55 +0000

    short

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section>
+      <div className="container">
+        <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+
+            {/*================= about img =============*/}
+            <div className="relative"></div>
+
+
+            
+        </div>
+      </div>  
+    </section>
   )
 }
 
