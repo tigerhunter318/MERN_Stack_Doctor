aaff210106744117f815e9aa4611af3c36cd5cbc
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-26 18:03:31 +0000

    short

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import aboutImg from '../../assets/images/about.png'
 import aboutCardImg from '../../assets/images/about-card.png'
@@ -20,6 +21,21 @@ const About = () => {
             {/*================= about content =============*/}
             <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
                 <h2 className="heading">Proud to be one of the nations best</h2>
+                <p className="text__para">For 30 years in a row, U.S. News & World Report has recognized us
+                    as one of the best publics hospitals in the Nation and #1 in Texas.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsum?
+                </p>
+
+                <p className="text__para mt-[30px]">
+                    Our best is something we strike for each day, caring for our patients-not
+                    looking back at what we accomplished but towards what we can do
+                    tomorrow. Providing the best. Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Aliquid, modi ?
+                </p>
+
+                <Link to='/'>
+                    <button className="btn">Learn More</button>
+                </Link>
             </div>
 
 
