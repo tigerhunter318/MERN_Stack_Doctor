b038e4c7ff3cfe163b31ab0b5121229d90ea476b
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-26 21:44:23 +0000

    short

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -5,7 +5,7 @@ const ServiceCard = ({item, index}) => {
   const {name, desc, bgColor, textColor} = item
 
   return (
-    <div className="py-[30px] py-3 lg:px-5">
+    <div className="py-[30px] px-3 lg:px-5">
       <h2 className="text-[26px] leading-9 text-headingColor font-[700]">{name}</h2>
       <p className="text-[16px] leading-7 font-[400] text-textColor mt-4 ">{desc}</p>
     </div>
