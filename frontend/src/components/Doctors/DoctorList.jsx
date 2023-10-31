a65ef1e56345963a3c04c1c4f2b3e1f2a9c80f32
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-31 23:07:48 +0000

    short

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -4,7 +4,11 @@ import DoctorCard from './DoctorCard'
 const DoctorList = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] 
-    mt-[30px] lg:mt-[55px]" >{doctors.map((doctor, index) => <DoctorCard key={index} doctor={doctor}/>)}</div>
+    mt-[30px] lg:mt-[55px]" >
+        {doctors.map( doctor => (
+            <DoctorCard key={doctor.id} doctor={doctor}/>
+        ))}
+    </div>
   )
 }
 
