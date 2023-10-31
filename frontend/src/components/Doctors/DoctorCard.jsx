a638ffbaf5cb275995faeb9d911c40a9f126bb23
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-31 23:22:10 +0000

    short

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import starIcon from "../../assets/images/Star.png"
+import { Link } from "react-router-dom"
+import { BsArrowRight } from 'react-icons/bs'
 
-const DoctorCard = ({doctor}) => {
+const DoctorCard = ({ doctor }) => {
 
-    const {} = doctor
+    const {name, avgRating, totalRating, photo, specialization, totalPatients, hospital} = doctor
 
   return (
-    <div>DoctorCard</div>
+    <div className="p-3 lg:p-5">
+        <div>
+            <img src={photo} className="w-full" alt="" />
+        </div>
+
+        <h2 className="text-[18px] leading-[30px] lg:text-[26px] lg:leading-9 
+        text-headingColor font-[700] mt-3 lg:mt-5">
+            {name}
+        </h2>
+
+        <div className="mt-2 lg:mt-4 flex items-center justify-between">
+            <span></span>
+        </div>
+    </div>
   )
 }
 
