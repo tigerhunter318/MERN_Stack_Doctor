afec2872f6f7a25dc400881a61004f5e998e30c3
tigerhunter318 <tigerhunter318@gmail.com>
2023-10-31 23:57:26 +0000

    short

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -46,6 +46,14 @@ const DoctorCard = ({ doctor }) => {
                     At {hospital}
                 </p>
             </div>
+
+            <Link 
+                to='/doctors' 
+                className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E]
+                flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                <BsArrowRight className="group-hover:text-white w-6 h-5" />
+            </Link>
         </div>
 
     </div>
